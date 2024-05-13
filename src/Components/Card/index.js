4fc8d41e9e55a72262c77d104dc00e4869180661
DavidHSCruz <david.hs.cruz@gmail.com>
2024-05-13 19:15:54 -0300

    Atualiza Link

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -3,7 +3,7 @@ import styles from './Card.module.css'
 
 export default function Card({ title, descricao, img, to }) {
     return (
-        <Link className={styles.card} to={to}>
+        <Link className={styles.card} to={to} target="_blank">
             <div>
                 <img src={img} alt='' />
             </div>
