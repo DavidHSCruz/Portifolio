e62927f46f4bbbe8d967e9aa958ccb66e9e1808a
DavidHSCruz <david.hs.cruz@gmail.com>
2024-05-14 11:13:26 -0300

    Resolve bugs Banner

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,12 +1,8 @@
-import LinkDestacado from 'Components/LinkDestacado'
 import styles from './Footer.module.css'
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <nav>
-                <LinkDestacado />
-            </nav>
             <p><i>©</i> 2024 David Cruz</p>
         </footer>
     )
